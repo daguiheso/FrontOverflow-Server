eb41371c64c56fb1c6ab1bfc42ad52a754e6a0c3
daguiheso <daguiheso@gmail.com>
2018-03-11 19:30:17 -0500

    Install express and implemented

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import http from 'http'
 import Debug from 'debug'
+import app from './app'
 
 const PORT = 3000
 const debug = new Debug('front-overflow:root')
-
-const app = http.createServer((req, res) => {
-	debug('New request')
-	res.writeHead(200, { 'COntent-Type': 'text/plain'})
-	res.write('Hola desde FrontOverflow')
-	res.end()
-})
 
 app.listen(PORT, () => {
 	debug('Server running at port' + PORT)
